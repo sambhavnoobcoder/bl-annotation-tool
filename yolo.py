8966f202b4772118d1a797f1ce25d95e47e7c166
sambhavnoobcoder <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-01-14 17:20:17 +0530

    Update yolo.py
    saves the predicted image at  123/yolo_test and label at   123/yolo_test/labels

--- yolo.py
+++ yolo.py
@@ -4,7 +4,7 @@ model = YOLO('/Users/sambhavdixit/PycharmProjects/ 123/best_1.pt')
 conf_thresh = 0.25
 hide_conf = True
 img = cv2.imread('/Users/sambhavdixit/PycharmProjects/ 123/path_to_directory/test.jpg')
-results = model.predict(img, stream=True)
+results = model.predict(img, stream=True, save=True,save_txt = True ,project=" 123" ,name="yolo_test" ,exist_ok=True)
 for result in results:
     boxes = result.boxes.cpu().numpy() # get boxes on cpu in numpy
     for box in boxes: # iterate boxes
